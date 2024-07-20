9a7f17873be272eafad00431bdf599342a93aaef
DerinFibonachi <168823953+DerinFibonachi@users.noreply.github.com>
2024-07-20 17:38:54 +0700

    Add files via upload

---
+++ Tugas Praktikum 2/Divide.java
@@ -0,0 +1,21 @@
+package Transpose;
+
+class Divide {
+    public static int[][] divide(int[][] first, int[][] second) {
+        int row = first.length, column = first[0].length;
+        int[][] result = new int[row][column];
+
+        System.out.println("\nCalculator.Division of Matrices:\n");
+        for (int r = 0; r < row; r++)
+            for (int c = 0; c < column; c++) {
+                if (second[r][c] != 0) {
+                    result[r][c] = first[r][c] / second[r][c];
+                } else {
+                    result[r][c] = 0;  // Handle division by zero
+                }
+            }
+
+        Print2dArray.print2dArray(result);
+        return result;
+    }
+}
